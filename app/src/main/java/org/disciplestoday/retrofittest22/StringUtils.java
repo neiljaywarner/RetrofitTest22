c3d7debe7bfaa09d612201ab00be89f5912c2eea
Neil Jay Warner <Neil.J.Warner@aa.com>
2016-06-12 18:42:06 -0500

    Read test json from file

---
+++ app/src/main/java/org/disciplestoday/retrofittest22/StringUtils.java
@@ -0,0 +1,33 @@
+package org.disciplestoday.retrofittest22;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by neil on 6/12/16.
+ */
+public class StringUtils {
+
+    public static String convertStreamToString(InputStream is) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        reader.close();
+        return sb.toString();
+    }
+
+    public static String getStringFromFile(Context context, String filePath) throws Exception {
+        final InputStream stream = context.getResources().getAssets().open(filePath);
+
+        String ret = convertStreamToString(stream);
+        //Make sure you close all streams.
+        stream.close();
+        return ret;
+    }
+}
